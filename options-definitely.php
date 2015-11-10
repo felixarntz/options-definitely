c0968dd250d6e2f690a9e329039d9434ed3c6803
Felix Arntz <felix-arntz@t-online.de>
2015-11-10 01:23:44 +0100

    fixed license

--- options-definitely.php
+++ options-definitely.php
@@ -6,8 +6,8 @@ Description: This framework plugin makes adding options to the WordPress admin a
 Version: 0.5.0
 Author: Felix Arntz
 Author URI: http://leaves-and-love.net
-License: GNU General Public License v2
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+License: GNU General Public License v3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: options-definitely
 Domain Path: /languages/
 Tags: wordpress, plugin, framework, library, developer, options, admin, backend, ui
